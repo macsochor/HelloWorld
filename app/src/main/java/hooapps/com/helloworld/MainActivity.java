55d57cf0b08625c851927c21600fc184daac6ea5
MSMD <MSMD@MSMDs-MacBook-Pro.local>
2017-09-21 12:38:32 -0400

    Initial commit

---
+++ app/src/main/java/hooapps/com/helloworld/MainActivity.java
@@ -0,0 +1,32 @@
+package hooapps.com.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hello " + editText.getText() + ", welcome to Android!");
+            }
+        });
+    }
+}
